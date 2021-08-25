a6e594ab37c1980889d21b79f0b8eb7dba93b025
Lillian <80014504+Lillianniu3640@users.noreply.github.com>
2021-08-25 20:04:06 +0800

    Update AdminMessages.vue

--- src/views/AdminMessages.vue
+++ src/views/AdminMessages.vue
@@ -5,7 +5,32 @@ b-container#adminmessages
       h4.text-center 訊息管理
     b-col(cols="12")
       b-img(src='../assets/images/line.png' style='width:100%')
+    b-col.mt-5.ml-4(cols="12")
+      b-table(:fields="fields" ref="messageTable")
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      fields: [
+        {
+          key: 'name',
+          label: '姓名'
+        },
+        {
+          key: 'email',
+          label: '信箱'
+        },
+        {
+          key: 'message',
+          label: '訊息'
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 #adminmessages
@@ -15,7 +40,7 @@ b-container#adminmessages
   position absolute
   top 0
   left 0
-  transform translate(0%,50%)
+  transform translate(0%,30%)
 h4
   display flex
   padding-left 2rem
