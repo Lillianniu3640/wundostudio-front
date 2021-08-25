922d5b8530a1d17f330c1e58473d9abc848ac314
Lillian <80014504+Lillianniu3640@users.noreply.github.com>
2021-08-25 20:19:37 +0800

    Update WorkCard.vue

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   b-card-group
-    b-card.work-card.mt-5.mr-2.text-dark(
+    b-card.work-card.mt-5.mr-2.text-light(
       overlay
       img-top
       :img-src="work.image"
