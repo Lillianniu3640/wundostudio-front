d27a9a5fb685c536d06b139081f8c40867ec38f6
Lillian <80014504+Lillianniu3640@users.noreply.github.com>
2021-08-25 18:51:29 +0800

    reset contact form in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -183,6 +183,11 @@ export default {
         title: 'Success',
         text: 'Thank you, I would reply soon.'
       })
+      this.message = {
+        name: '',
+        email: '',
+        text: ''
+      }
     }
   },
   components: {
